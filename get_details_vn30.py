cb623268a8b81b4797f714e17ebcbc3e56ab3309
Hieu Nguyen <hieunguyen@QCF-27s-iMac.local>
2019-07-17 22:47:56 +0700

    aaa new vn 30

--- get_details_vn30.py
+++ get_details_vn30.py
@@ -16,11 +16,13 @@ from newspaper import fulltext
 from tqdm import tqdm
 from tqdm import tnrange, tqdm_notebook
 
-logging.basicConfig(filename="crawl.log",level=logging.ERROR)
+logging.basicConfig(filename="crawlvn30.log",level=logging.ERROR)
 
 news_url = pd.read_csv("news_url.csv", usecols = range(1,5))
 
+news_url_mini = news_url[news_url['ticker'].isin(['CII','CTD','CTG','DHG','DPM','EIB','FPT','GAS','GMD','HDB','HPG','MBB','MSN','MWG','NVL','PNJ','REE','ROS','SAB','SBT','SSI','STB','TCB','VCB','VHM','VJC','VIC','VPB','VRE','VNM'])]
 
+# print(news_url_mini)
 def get_details_from_url(df_to_get):
     data_dicts = []
     for newsdate, url in tqdm(zip(df_to_get['newsdate'].values, df_to_get['url'].values)):
@@ -37,14 +39,14 @@ def get_details_from_url(df_to_get):
         except Exception as e:
 #             logging.error()
             continue
+#         print(data_dicts)
     return data_dicts
 
+#from multiprocessing.dummy import Pool as ThreadPool 
+#pool = ThreadPool(32)
+#results = pool.map(get_details_from_url(news_url_mini))
+#pool.close() 
+#pool.join()
 
-from multiprocessing.dummy import Pool as ThreadPool 
-pool = ThreadPool(32)
-results = pool.map(get_details_from_url(news_url))
-pool.close() 
-pool.join()
-
-newsfullmarket = pd.DataFrame(results)
-newsfullmarket.to_csv("newsfullmarket.csv")
+newsvn30 = pd.DataFrame(get_details_from_url(news_url_mini))
+newsvn30.to_csv('newsvn30.csv')
