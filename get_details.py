b51ac1f16c06cd7c47c1bc7da5b6dd99e9527ed0
Nguyenhieuqcf <nguyenhieuec@gmail.com>
2019-07-17 21:52:39 +0700

    add py file for run in terminal

---
+++ get_details.py
@@ -0,0 +1,47 @@
+
+import pandas as pd
+import numpy as np
+
+#internet package
+
+import urllib3
+from bs4 import BeautifulSoup  
+http = urllib3.PoolManager()
+
+#functional package
+import logging
+import requests
+from newspaper import Article
+from newspaper import fulltext
+from tqdm import tqdm
+from tqdm import tnrange, tqdm_notebook
+
+logging.basicConfig(filename="crawl.log",level=logging.ERROR)
+
+news_url = pd.read_csv("news_url.csv", usecols = range(1,5))
+
+
+def get_details_from_url(df_to_get):
+    data_dicts = []
+    for newsdate, url in tqdm(zip(df_to_get['newsdate'].values, df_to_get['url'].values)):
+        row_dict = {}
+        try:
+            article = Article(url, language='vi')
+            article.download()
+            article.parse()
+            article.nlp()
+            row_dict['newsdate'] = newsdate
+            row_dict['summary'] = article.summary
+            row_dict['keywords'] = article.keywords
+            data_dicts.append(row_dict)
+        except Exception as e:
+#             logging.error()
+            continue
+#         print(data_dicts)
+    return data_dicts
+
+from multiprocessing.dummy import Pool as ThreadPool 
+pool = ThreadPool(32)
+results = pool.map(get_details_from_url(news_url))
+pool.close() 
+pool.join()
