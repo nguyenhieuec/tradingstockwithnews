2207a87d9846c929dd1fa9953575f7a67067ca2d
Nguyen Hieu QCF <nguyenhieuec@gmail.com>
2019-08-21 22:22:09 +0700

    start BERT-AS-SERVICE file

--- runbert.py
+++
@@ -1,3 +0,0 @@
-from bert_serving.client import BertClient
-bc = BertClient()
-bc.encode(['First do it', 'then do it right', 'then do it better'])
---
+++ runbert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bert-serving-start -model_dir /home/hieu/Desktop/multi_cased_L-12_H-768_A-12
